078ec3d829def99d583f7d9e23dc3d4587d10507
iBomani <robertkosztandi3013@gmail.com>
2024-11-04 19:28:40 +0100

    add imports, with explanation

--- train_model.py
+++ train_model.py
@@ -1,5 +1,13 @@
 import pandas as pd 
 
+from sklearn.feature_extraction.text import TfidfVectorizer # tfidf -> term frequency inverse document frequency, szavak fontossagat meri
+from sklearn.model_selection import train_test_split # a dataset-et szetbontja train es test reszre
+from sklearn.naive_bayes import MultinomialNB # Naive Bayes modell
+from sklearn.pipeline import make_pipeline # pipeline, ami osszefuzi a tfidf-et es a modellt
+import pickle # modell mentese 
+
 data = pd.read_csv('tweet_emotions.csv') # kaggle-rol van a dataset
 
-print(data.head(10))
+# adatok feldolgozasa, szetvalasztasa
+texts = data['content'].fillna('')
+labels = data['sentiment']
